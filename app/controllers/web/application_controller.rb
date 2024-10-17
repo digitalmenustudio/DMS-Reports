05a457d3a93c3750f65de5443e3e41ce8b3944ca
SimSimSim <digitalmenustudio@gmail.com>
2024-10-17 12:33:55 -0400

    authentification required

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,5 +1,7 @@
 module Web
   class ApplicationController < ::ApplicationController
     layout "web/layouts/application"
+    before_action :authenticate_user!
+
   end
 end
--- app/controllers/web/sites_controller.rb
+++ app/controllers/web/sites_controller.rb
@@ -1,7 +1,6 @@
 module Web
   class SitesController < ApplicationController
     before_action { authorize :sites }
-    before_action :authenticate_user!
 
     def home
     end
